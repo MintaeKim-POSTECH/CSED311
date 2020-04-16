bba4f832e305027efcc173ede0d8b12103b97e1c
Mintae Kim <shsesther928@postech.ac.kr>
2020-04-16 19:17:03 +0900

    3-2: Main Pipeline Started - KMT

--- lab3/cpu.v
+++ lab3/cpu.v
@@ -9,21 +9,52 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	input inputReady;								
 	input reset_n;									
 	input clk;
+
+	reg [`WORD_SIZE-1:0] address;
+	reg [`WORD_SIZE-1:0] PC;
+	reg readM, writeM;
+	reg CS, NS; // Keep
 	
 	initial begin // Initial Logic
 		assign data = 16'bz;
+		assign PC = 0;
+		assign CS = 0;
+		assign NS = 0;
 	end
 
-	always (*) begin // Combinational Logic
+	always @(*) begin // Calculation: Combinational Logic
+		if (CS == 1) begin // TODO: ID (Instruction Decoding)
+			
+		end
+		else begin // TODO: MEM (Memory Access Stage)
+		end
 	end
 
+	always @(posedge inputRready) begin // Clock I Starting Point: Combinational Logic
+		CS <= NS;
+	end
+	always @(posedge ackOutput) begin // Clock II Starting Point: Combinational Logic
+		CS <= NS;
+	end
+
+	
 	always @(posedge clk) begin // Clock I
+		if (CS == 0) begin
+			readM <= 1;
+			writeM <= 0;
+			NS <= 1;
+		end
 	end
-
 	always @(negedge clk) begin // Clock II
+		if (CS == 1) begin
+			readM <= 0;
+			writeM <= 1;
+			NS <= 0;
+		end
 	end
+	
 
 	always @(negedge reset_n) begin // Reset Activated
-	end
-																																					  
+
+	end																																				  
 endmodule							  																		  
