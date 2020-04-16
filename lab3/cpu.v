9934431a2bc183ee045f72405fba7e28d381e6eb
Mintae Kim <shsesther928@postech.ac.kr>
2020-04-16 16:41:29 +0900

    3-1: Frame

--- lab3/cpu.v
+++ lab3/cpu.v
@@ -8,6 +8,22 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	input ackOutput;								
 	input inputReady;								
 	input reset_n;									
-	input clk;			
+	input clk;
+	
+	initial begin // Initial Logic
+		assign data = 16'bz;
+	end
+
+	always (*) begin // Combinational Logic
+	end
+
+	always @(posedge clk) begin // Clock I
+	end
+
+	always @(negedge clk) begin // Clock II
+	end
+
+	always @(negedge reset_n) begin // Reset Activated
+	end
 																																					  
 endmodule							  																		  
