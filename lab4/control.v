e7b122994cc20a8406cc705c29be20468dee60f8
YoungtaeMoon <moonyoungtae@postech.ac.kr>
2020-04-29 22:30:00 +0900

    lab3_ control module refactoring finished

--- lab4/control.v
+++ lab4/control.v
@@ -3,16 +3,10 @@
 module control(inst, RegDest, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, Reg2Save, PCSrc1, PCSrc2);
 	input [`WORD_SIZE-1:0] inst;
 	output reg RegDest, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, Reg2Save, PCSrc1, PCSrc2;
-	reg isJAL, isJRL, isJPR, isLoad, isStore;
 
 	reg [3:0] opcode;
 
 	initial begin
-		isJAL = 0;
-		isJRL = 0;
-		isJPR = 0;
-		isLoad = 0;
-		isStore = 0;
 		RegDest = 0;
 		MemRead = 0;
 		MemtoReg = 0;
@@ -25,11 +19,6 @@ module control(inst, RegDest, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, Reg
 	end
 
 	always @(*) begin
-		isJAL = 0;
-		isJRL = 0;
-		isJPR = 0;
-		isLoad = 0;
-		isStore = 0;
 		RegDest = 0;
 		MemRead = 0;
 		MemtoReg = 0;
@@ -41,55 +30,51 @@ module control(inst, RegDest, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, Reg
 		PCSrc2 = 0;
 	
 		opcode=inst[15:12];
-		if(opcode<=8)begin
-			// ALUSrc: Using Immediate Values? / Branch Exception
-			if (opcode <= 3) ALUSrc = 0;
-			else ALUSrc = 1;
-	
-			if(opcode==7)begin
-				isLoad = 1;
-			end
-			else if(opcode==8)begin
-				isStore = 1;
-			end
-		end
-		// PCSrc1: isJtype?
-		else if(opcode==9)begin
-			PCSrc1 = 1;
-		end
-		else if(opcode==10)begin
-			PCSrc1 = 1;
-			isJAL = 1;
-		end
-		else if (opcode==15)begin
-			ALUSrc = 0;
-			// RegDest: R Type (rd) vs I Type (rt)
+
+		//RegDest: R Type (rd) vs I Type (rt)
+		if (opcode==15)begin
 			RegDest = 1; 
-			if(inst[5:0]==25)begin
-				isJPR = 1;
-			end
-			else if(inst[5:0]==26)begin
-				isJRL = 1;
-			end
 		end
 
-		// RegWrite
-		if (isStore == 1) RegWrite = 0;
-		else if (opcode >= 0 && opcode <= 3) RegWrite = 0;
-		else if (opcode == 9 || (opcode == 15 && inst[5:0] == 25)) RegWrite = 0;
-		else RegWrite = 1;
+		//MemRead, MemtoReg: True for Load Instruction
+		if (opcode==7)begin
+			MemRead = 1;
+			MemtoReg = 1;
+		end
 
-		// Reg2Save: Instructions that Saves PC in $2 / (isJAL || isJRL)
-		Reg2Save = (isJAL || isJRL);
+		//MemWrite: True for Store Instruction(opcode 8)
+		if (opcode==8)begin
+			MemWrite = 1;
+		end
 
-		// PCSrc2: (isJRL || isJPR)
-		PCSrc2 = (isJRL || isJPR);
+		//ALUSrc: True when opcode is 4~8 
+		if(opcode > 3&&opcode <= 8) begin
+		ALUSrc = 1;
+		end
 
-		// MemtoReg: isLoad
-		MemtoReg = isLoad;
-		// MemWrite: isStore
-		MemWrite = isStore;
-		// MemRead: isLoad
-		MemRead = isLoad;
+		//RegWrite: True for opcode 4~7, 10~14 and False when
+		if (opcode>=4&&opcode<=7 || opcode>=10&&opcode<=15)begin
+			if(opcode==15&&inst[5:0]==25) RegWrite = 0;
+			else RegWrite = 1;
+		end
+
+		//Reg2Save: isJAL || isJRL, 
+		// JAL: opcode 10
+		// JRL: opcode 15 and function code 26
+		if (opcode==10||(opcode==15&&inst[5:0]==26))begin
+			Reg2Save=1;
+		end
+
+		//PCSrc1
+		if (opcode==9||opcode==10)begin
+			PCSrc1 = 1;
+		end
+
+		//PCSrc2: isJRL || isJPR
+		//JRL: opcode 15 and function code 26
+		//JPR: opcode 15 and function code 25
+		if (opcode==15)begin
+			if(inst[5:0]==25||inst[5:0]==26) PCSrc2 = 1;
+		end
 	end
 endmodule
